58dd2f6c92d4db908b80833e6e3ff8daa6fbb496
harisakthi301104 <harisakthi301104@gmail.com>
2026-04-14 19:30:24 +0530

    Commiting Report Module

---
+++ Expense_Tracker/DTO/ReportFileterDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace Expense_Tracker.DTO
+{
+    public class ReportFileterDTO
+    {
+        public int UserId { get; set; }
+
+        public DateTime? StartDate { get; set; }
+
+        public DateTime? EndDate { get; set; }
+
+        public string? Category { get; set; }
+    }
+}
---
+++ Expense_Tracker/DTO/SummaryDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Expense_Tracker.DTO
+{
+    public class SummaryDTO
+    {
+       
+            public decimal TotalExpense { get; set; }
+
+            public decimal DailyAverage { get; set; }
+
+            public string Insight { get; set; }
+        }
+}
