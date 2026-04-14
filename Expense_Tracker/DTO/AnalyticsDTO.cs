726446afb80b8ef593457dda4bf583af6817ce14
harisakthi301104 <harisakthi301104@gmail.com>
2026-04-14 20:02:52 +0530

    Commiting Analytics Module

---
+++ Expense_Tracker/DTO/AnalyticsDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace Expense_Tracker.DTO
+{
+    public class AnalyticsDTO
+    {
+        public decimal DailyAverage { get; set; }
+
+        public string TopCategory { get; set; }
+
+        public decimal TopCategoryAmount { get; set; }
+
+        public string SpendingTrend { get; set; }
+
+        public string Insight { get; set; }
+    }
+}
