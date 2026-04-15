81e70cec23a129bc928306503bac43005cd515a0
harisakthi301104 <harisakthi301104@gmail.com>
2026-04-15 11:42:49 +0530

    Commiting Auth Service and AuthController

---
+++ Expense_Tracker/Services/AuthService.cs
@@ -0,0 +1,81 @@
+﻿using Expense_Tracker.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Expense_Tracker.Services
+{
+    public class AuthService
+    {
+        private readonly AppDbContext _context;
+        private readonly IConfiguration _config;
+
+        public AuthService(AppDbContext context, IConfiguration config)
+        {
+            _context = context;
+            _config = config;
+        }
+
+        public async Task<string> RegisterAsync(RegisterDto dto)
+        {
+            bool exists = await _context.Users
+                .AnyAsync(u => u.Email == dto.Email);
+
+            if (exists)
+                throw new Exception("Email already registered.");
+
+            var user = new User
+            {
+                FullName = dto.FullName,
+                Email = dto.Email,
+                PasswordHash = BCrypt.Net.BCrypt.HashPassword(dto.Password),
+                Role = dto.Role,
+                CreatedAt = DateTime.UtcNow
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return GenerateToken(user);
+        }
+
+        public async Task<string> LoginAsync(LoginDto dto)
+        {
+            var user = await _context.Users
+                .FirstOrDefaultAsync(u => u.Email == dto.Email);
+
+            if (user == null || !BCrypt.Net.BCrypt.Verify(dto.Password, user.PasswordHash))
+                throw new Exception("Invalid email or password.");
+
+            return GenerateToken(user);
+        }
+
+        private string GenerateToken(User user)
+        {
+            var key = new SymmetricSecurityKey(
+                             Encoding.UTF8.GetBytes(_config["JwtSettings:SecretKey"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var expiry = DateTime.UtcNow.AddDays(7);
+
+            var claims = new[]
+            {
+            new Claim(ClaimTypes.NameIdentifier, user.UserId.ToString()),
+            new Claim(ClaimTypes.Email,          user.Email),
+            new Claim(ClaimTypes.Role,           user.Role)
+        };
+
+            var token = new JwtSecurityToken(
+                issuer: _config["JwtSettings:Issuer"],
+                audience: _config["JwtSettings:Audience"],
+                claims: claims,
+                expires: expiry,
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+    }
+}
